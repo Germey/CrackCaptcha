ead45b98116730b69dc0274136b157600afa3c78
Germey <cqc@cuiqingcai.com>
2018-02-19 02:40:06 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from os.path import join, exists
 from os import makedirs
 import pickle
 import math
-
 from sklearn.model_selection import train_test_split
 
 FLAGS = None
@@ -46,28 +45,10 @@ def get_data(data_x, data_y):
     return train_x, train_y, dev_x, dev_y, test_x, test_y
 
 
-def weight(shape, stddev=0.1, mean=0):
-    initial = tf.truncated_normal(shape=shape, mean=mean, stddev=stddev)
-    return tf.Variable(initial)
-
-
-def bias(shape, value=0.1):
-    initial = tf.constant(value=value, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(input, filter, strides=[1, 1, 1, 1], padding='SAME'):
-    return tf.nn.conv2d(input, filter, strides=strides, padding=padding)
-
-
-def max_pool(input, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME'):
-    return tf.nn.max_pool(input, ksize=ksize, strides=strides, padding=padding)
-
-
 def main():
     data_x, data_y = load_data()
+    n_classes = data_y.shape[-1]
     train_x, train_y, dev_x, dev_y, test_x, test_y = get_data(data_x, data_y)
-    
     train_steps = math.ceil(train_x.shape[0] / FLAGS.train_batch_size)
     dev_steps = math.ceil(dev_x.shape[0] / FLAGS.dev_batch_size)
     test_steps = math.ceil(test_x.shape[0] / FLAGS.test_batch_size)
@@ -75,7 +56,7 @@ def main():
     global_step = tf.Variable(-1, trainable=False, name='global_step')
     
     # train and dev dataset
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y)).shuffle(10000)
     train_dataset = train_dataset.batch(FLAGS.train_batch_size)
     
     dev_dataset = tf.data.Dataset.from_tensor_slices((dev_x, dev_y))
@@ -97,109 +78,72 @@ def main():
     with tf.variable_scope('inputs'):
         # x.shape = [-1, 60, 160, 3]
         x, y_label = iterator.get_next()
-    print(x.shape)
     
     keep_prob = tf.placeholder(tf.float32, [])
-    print('keepprob', keep_prob)
-    # layer1
-    w_conv1 = weight([3, 3, 3, 32])
-    b_conv1 = bias([32])
-    # h_conv1.shape: [-1, 60, 160, 32]
-    h_conv1 = tf.nn.relu(conv2d(x, w_conv1) + b_conv1)
-    print('H Conv1', h_conv1)
-    # h_pool1.shape: [-1, 30, 80, 32]
-    h_pool1 = max_pool(h_conv1)
-    print('H Pool1', h_pool1)
-    h_drop1 = tf.nn.dropout(h_pool1, keep_prob)
-    print('H Drop1', h_drop1)
     
-    # layer2
-    w_conv2 = weight([3, 3, 32, 64])
-    b_conv2 = bias([64])
-    # h_conv2.shape: [-1, 30, 80, 64]
-    h_conv2 = tf.nn.relu(conv2d(h_drop1, w_conv2) + b_conv2)
-    # h_pool2.shape: [-1, 15, 40, 64]
-    h_pool2 = max_pool(h_conv2)
-    h_drop2 = tf.nn.dropout(h_pool2, keep_prob)
-    print('H Drop2', h_drop2)
-    # layer3
-    w_conv3 = weight([3, 3, 64, 64])
-    b_conv3 = bias([64])
-    # h_conv3.shape: [-1, 15, 40, 64]
-    h_conv3 = tf.nn.relu(conv2d(h_drop2, w_conv3) + b_conv3)
-    # h_pool3.shape: [-1, 8, 20, 64]
-    h_pool3 = max_pool(h_conv3)
-    h_drop3 = tf.nn.dropout(h_pool3, keep_prob)
-    print('H Drop3', h_drop3)
+    y = tf.cast(x, tf.float32)
     
-    h_reshape = tf.reshape(h_drop3, [-1, 8 * 20 * 64])
-    # fully connected layer1
-    w_f1 = weight([8 * 20 * 64, 1024])
-    b_f1 = bias([1024])
-    # h_f1.shape: [batch_size, 1024]
-    h_f1 = tf.nn.relu(tf.matmul(h_reshape, w_f1) + b_f1)
-    h_d1 = tf.nn.dropout(h_f1, keep_prob)
+    # 3 CNN layers
+    for _ in range(3):
+        y = tf.layers.conv2d(y, filters=32, kernel_size=3, padding='same', activation=tf.nn.relu)
+        y = tf.layers.max_pooling2d(y, pool_size=2, strides=2, padding='same')
+        y = tf.layers.dropout(y, rate=keep_prob)
     
-    print('H D1', h_d1)
+    # 2 dense layers
+    y = tf.layers.flatten(y)
+    y = tf.layers.dense(y, 1024, activation=tf.nn.relu)
+    y = tf.layers.dropout(y, rate=keep_prob)
+    y = tf.layers.dense(y, n_classes)
     
-    # fully connected layer2
-    w_f2 = weight([1024, CAPTCHA_LENGTH * VOCAB_LENGTH])
-    b_f2 = bias([CAPTCHA_LENGTH * VOCAB_LENGTH])
-    # h_f2.shape: [batch_size, CAPTCHA_LENGTH * VOCAB_LENGTH]
-    h_f2 = tf.nn.relu(tf.matmul(h_d1, w_f2) + b_f2)
-    h_d2 = tf.nn.dropout(h_f2, keep_prob)
-    
-    h_f2_reshape = tf.reshape(h_d2, [-1, VOCAB_LENGTH])
+    y_reshape = tf.reshape(y, [-1, VOCAB_LENGTH])
     y_label_reshape = tf.reshape(y_label, [-1, VOCAB_LENGTH])
     
     # loss
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=h_f2_reshape, labels=y_label_reshape))
+    cross_entropy = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=y_reshape, labels=y_label_reshape))
     
-    max_index_predict = tf.argmax(h_f2_reshape, axis=-1)
-    print('Max Index Predict', max_index_predict)
+    # accuracy
+    max_index_predict = tf.argmax(y_reshape, axis=-1)
     max_index_label = tf.argmax(y_label_reshape, axis=-1)
-    print('Max Index Label', max_index_label)
-    
     correct_predict = tf.equal(max_index_predict, max_index_label)
-    print('Correct predict', correct_predict)
     accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
     
-    # Train
-    train = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy, global_step=global_step)
-    # Saver
+    # train
+    train_op = tf.train.RMSPropOptimizer(FLAGS.learning_rate).minimize(cross_entropy, global_step=global_step)
+    
+    # saver
     saver = tf.train.Saver()
     
-    # Iterator
+    # iterator
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     
-    # Global step
+    # global step
     gstep = 0
     
+    # checkpoint dir
+    if not exists(FLAGS.checkpoint_dir):
+        makedirs(FLAGS.checkpoint_dir)
+    
     if FLAGS.train:
-        
-        if tf.gfile.Exists(FLAGS.summaries_dir):
-            tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
-        
         for epoch in range(FLAGS.epoch_num):
             tf.train.global_step(sess, global_step_tensor=global_step)
-            # Train
+            # train
             sess.run(train_initializer)
             for step in range(int(train_steps)):
-                loss, acc, gstep, _ = sess.run([cross_entropy, accuracy, global_step, train],
+                loss, acc, gstep, _ = sess.run([cross_entropy, accuracy, global_step, train_op],
                                                feed_dict={keep_prob: FLAGS.keep_prob})
-                # Print log
+                # print log
                 if step % FLAGS.steps_per_print == 0:
                     print('Global Step', gstep, 'Step', step, 'Train Loss', loss, 'Accuracy', acc)
             
             if epoch % FLAGS.epochs_per_dev == 0:
-                # Dev
+                # dev
                 sess.run(dev_initializer)
                 for step in range(int(dev_steps)):
                     if step % FLAGS.steps_per_print == 0:
                         print('Dev Accuracy', sess.run(accuracy, feed_dict={keep_prob: 1}), 'Step', step)
             
-            # Save model
+            # save model
             if epoch % FLAGS.epochs_per_save == 0:
                 saver.save(sess, FLAGS.checkpoint_dir, global_step=gstep)
     
@@ -209,25 +153,29 @@ def main():
         saver.restore(sess, ckpt.model_checkpoint_path)
         print('Restore from', ckpt.model_checkpoint_path)
         sess.run(test_initializer)
-
+        for step in range(int(test_steps)):
+            if step % FLAGS.steps_per_print == 0:
+                print('Test Accuracy', sess.run(accuracy, feed_dict={keep_prob: 1}), 'Step', step)
+        
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Captcha')
-    parser.add_argument('--train_batch_size', help='train batch size', default=200)
-    parser.add_argument('--dev_batch_size', help='dev batch size', default=50)
-    parser.add_argument('--test_batch_size', help='test batch size', default=500)
+    parser.add_argument('--train_batch_size', help='train batch size', default=128)
+    parser.add_argument('--dev_batch_size', help='dev batch size', default=256)
+    parser.add_argument('--test_batch_size', help='test batch size', default=256)
     parser.add_argument('--source_data', help='source size', default='./data/data.pkl')
     parser.add_argument('--num_layer', help='num of layer', default=2, type=int)
     parser.add_argument('--num_units', help='num of units', default=64, type=int)
     parser.add_argument('--time_step', help='time steps', default=32, type=int)
     parser.add_argument('--embedding_size', help='time steps', default=64, type=int)
     parser.add_argument('--category_num', help='category num', default=5, type=int)
-    parser.add_argument('--learning_rate', help='learning rate', default=0.0001, type=float)
+    parser.add_argument('--learning_rate', help='learning rate', default=0.001, type=float)
     parser.add_argument('--epoch_num', help='num of epoch', default=10000, type=int)
     parser.add_argument('--epochs_per_test', help='epochs per test', default=100, type=int)
     parser.add_argument('--epochs_per_dev', help='epochs per dev', default=2, type=int)
-    parser.add_argument('--epochs_per_save', help='epochs per save', default=100, type=int)
-    parser.add_argument('--steps_per_print', help='steps per print', default=2, type=int)
+    parser.add_argument('--epochs_per_save', help='epochs per save', default=10, type=int)
+    parser.add_argument('--steps_per_print', help='steps per print', default=4, type=int)
     parser.add_argument('--steps_per_summary', help='steps per summary', default=100, type=int)
     parser.add_argument('--keep_prob', help='train keep prob dropout', default=0.5, type=float)
     parser.add_argument('--checkpoint_dir', help='checkpoint dir', default='ckpt/model.ckpt', type=str)
